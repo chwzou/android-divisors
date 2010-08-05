2938dd3e38693372d6b57f67f5667d5cf1144605
Vince Mi <vinc3m1@gmail.com>
2010-08-05 01:56:35 -0500

    loop test run

--- src/com/makeramen/bumpy/BumpyActivity.java
+++ src/com/makeramen/bumpy/BumpyActivity.java
@@ -1,13 +1,63 @@
 package com.makeramen.bumpy;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class BumpyActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	
+	TextView mTextView;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		mTextView = (TextView) findViewById(R.id.text);
+		
+		// performance test
+		// currently takes almost 3 minutes...
+		
+		new DivisorTask().execute();
+		
+	}
+	
+	private class DivisorTask extends AsyncTask<Void, Integer, Void> {
+
+		long startTime;
+		
+		@Override
+		protected void onPreExecute() {
+			startTime = System.currentTimeMillis();
+		}
+		
+		@Override
+		protected Void doInBackground(Void... params) {
+			for (int i = 1; i <= 10000; i++) {
+				double s = Math.sqrt(i);
+				for (int j = 1; j <= s; j++) {
+					if (i%j==0)
+					publishProgress(i, j);
+				}
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			Log.d("bumpy", "i: " + progress[0] + " j: " + progress[1]);
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			Log.d("bumpy", "done!");
+			Log.d("bumpy", "time elapsed(ms): " + (System.currentTimeMillis() - startTime) );
+			
+		}
+	}
+	
 }
