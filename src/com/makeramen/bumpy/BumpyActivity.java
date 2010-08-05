41feb86e446112dd85761260a68444e9ee42a25c
Vince Mi <vinc3m1@gmail.com>
2010-08-04 23:18:12 -0500

    initial commit

---
+++ src/com/makeramen/bumpy/BumpyActivity.java
@@ -0,0 +1,13 @@
+package com.makeramen.bumpy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BumpyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
