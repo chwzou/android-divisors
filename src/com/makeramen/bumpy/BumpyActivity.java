5d66aac46cbe9672b3696a51f049945216d2fcc7
Vince Mi <vinc3m1@gmail.com>
2010-08-07 05:41:07 -0500

    wooo headers working

--- src/com/makeramen/bumpy/BumpyActivity.java
+++ src/com/makeramen/bumpy/BumpyActivity.java
@@ -1,15 +1,30 @@
 package com.makeramen.bumpy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 public class BumpyActivity extends Activity {
 	
 	TextView text;
+	ListView list;
 	
-	int[] factors = new int[10000];
+	public static final String LABEL_DIVISORS = " divisors";
+	public static final String LABEL_PRIME = "prime number";
+	public static final String KEY_NUMBER = "number";
+	public static final String KEY_DESCRIPTION = "description";
+	
+	SimpleAdapter adapter;
+	
+//	int[] factors = new int[10000];
+	
+	ArrayList<HashMap<String, String>> divisors = new ArrayList<HashMap<String, String>>();
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -18,6 +33,14 @@ public class BumpyActivity extends Activity {
 		setContentView(R.layout.main);
 		
 		text = (TextView) findViewById(R.id.text);
+		list = (ListView) findViewById(R.id.list);
+		
+		adapter = new SimpleAdapter(
+				this,
+				divisors,
+				android.R.layout.simple_list_item_2,
+				new String[] {KEY_NUMBER, KEY_DESCRIPTION},
+				new int[] {android.R.id.text1, android.R.id.text2});
 		
 		new DivisorTask().execute();
 		
@@ -27,6 +50,8 @@ public class BumpyActivity extends Activity {
 
 		long startTime;
 		int count = 0;
+		int fcount;
+		HashMap<String, String> mMap;
 		
 		@Override
 		protected void onPreExecute() {
@@ -37,20 +62,24 @@ public class BumpyActivity extends Activity {
 		protected Void doInBackground(Void... params) {
 			for (int n = 1; n <= 10000; n++) {
 				
-				factors[n-1] = 0;
+				fcount = 0;
 				
 				double sq = Math.sqrt(n);
 				
 				for (int f = 1; f <= sq; f++) {
 					
 					if (n % f == 0) {
-						factors[n-1] += ((f == sq) ? 1 : 2); 
+						fcount += ((f == sq) ? 1 : 2);
 					}
 					
 				}
+
+				mMap = new HashMap<String , String>();
+				mMap.put(KEY_NUMBER, Integer.toString(n));
 				
-				count += factors[n-1];
+				mMap.put(KEY_DESCRIPTION, (fcount < 3) ? LABEL_PRIME :(Integer.toString(fcount) + LABEL_DIVISORS));
 				
+				divisors.add(mMap);
 			}
 			
 			return null;
@@ -66,6 +95,7 @@ public class BumpyActivity extends Activity {
 			text.append("\ntime elapsed(ms): " + (System.currentTimeMillis() - startTime));
 			text.append("\ntotal number of factors: " + count);
 			
+			list.setAdapter(adapter);
 		}
 	}
 	
