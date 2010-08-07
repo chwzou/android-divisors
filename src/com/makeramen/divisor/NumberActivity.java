ba1c8134c2ccab196041f00ab9685e3b9abe26c6
Vincent Mi <vinc3m1@gmail.com>
2010-08-07 14:28:35 -0500

    added progressdialog and renamed

--- src/com/makeramen/divisor/NumberActivity.java
+++ src/com/makeramen/divisor/NumberActivity.java
@@ -1,19 +1,22 @@
-package com.makeramen.bumpy;
+package com.makeramen.divisor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
-public class BumpyActivity extends Activity {
+public class NumberActivity extends Activity {
 	
 	TextView text;
 	ListView list;
+	
+	ProgressDialog progressDialog;
 	
 	public static final String LABEL_DIVISORS = " divisors";
 	public static final String LABEL_PRIME = "prime number";
@@ -55,11 +58,22 @@ public class BumpyActivity extends Activity {
 		
 		@Override
 		protected void onPreExecute() {
+			
+			progressDialog = new ProgressDialog(NumberActivity.this);
+			progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			progressDialog.setMessage("Loading...");
+			progressDialog.setCancelable(false);
+			progressDialog.setMax(10000);
+			progressDialog.show();
+			
 			startTime = System.currentTimeMillis();
 		}
 		
 		@Override
 		protected Void doInBackground(Void... params) {
+			
+			int time = 0;
+			
 			for (int n = 1; n <= 10000; n++) {
 				
 				fcount = 0;
@@ -77,25 +91,32 @@ public class BumpyActivity extends Activity {
 				mMap = new HashMap<String , String>();
 				mMap.put(KEY_NUMBER, Integer.toString(n));
 				
-				mMap.put(KEY_DESCRIPTION, (fcount < 3) ? LABEL_PRIME :(Integer.toString(fcount) + LABEL_DIVISORS));
+				mMap.put(KEY_DESCRIPTION, (fcount == 2) ? LABEL_PRIME :(Integer.toString(fcount) + LABEL_DIVISORS));
 				
 				divisors.add(mMap);
+				
+				
+				if (n % 379 == 0 && System.currentTimeMillis() - startTime > time){
+					publishProgress(n);
+					time += 500;
+				}
+
 			}
 			
 			return null;
 		}
 		
-//		@Override
-//		protected void onProgressUpdate(Integer... progress) {
-//			Log.d("bumpy", "i: " + progress[0] + " j: " + progress[1]);
-//		}
+		@Override
+		protected void onProgressUpdate(Integer... progress) {
+			progressDialog.setProgress(progress[0]);
+		}
 		
 		@Override
 		protected void onPostExecute(Void result) {
 			text.append("\ntime elapsed(ms): " + (System.currentTimeMillis() - startTime));
 			text.append("\ntotal number of factors: " + count);
-			
 			list.setAdapter(adapter);
+			progressDialog.dismiss();
 		}
 	}
 	
