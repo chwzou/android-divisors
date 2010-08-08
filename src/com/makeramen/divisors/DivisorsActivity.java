4509fe0b3a3fc8299c505d3735e8271820674a03
Vincent Mi <vinc3m1@gmail.com>
2010-08-07 22:14:57 -0500

    begin divisors activity

---
+++ src/com/makeramen/divisors/DivisorsActivity.java
@@ -0,0 +1,16 @@
+package com.makeramen.divisors;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class DivisorsActivity extends Activity implements ListView.OnItemClickListener {
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
