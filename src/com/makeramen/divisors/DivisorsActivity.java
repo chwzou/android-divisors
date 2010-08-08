d347a3e34eeb5a5b48c33246a02cbf144a3899f2
Vince Mi <vinc3m1@gmail.com>
2010-08-08 00:36:23 -0500

    divisors view implemented

--- src/com/makeramen/divisors/DivisorsActivity.java
+++ src/com/makeramen/divisors/DivisorsActivity.java
@@ -1,16 +1,61 @@
 package com.makeramen.divisors;
 
-import android.app.Activity;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import android.app.ListActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class DivisorsActivity extends Activity implements ListView.OnItemClickListener {
-
+public class DivisorsActivity extends ListActivity implements ListView.OnItemClickListener {
+	
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		// TODO Auto-generated method stub
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		
+		setContentView(R.layout.divisors);
+		
+		int number = this.getIntent().getIntExtra(NumbersActivity.EXTRA_NUMBER, -1);
+//		divisors = this.getIntent().getIntExtra(NumbersActivity.EXTRA_DIVISORS, -1);
+		
+		ArrayList<Integer> divisors = new ArrayList<Integer>();
+		
+		this.setTitle("Divisors of " + number + ":");
+		
+		double sq = Math.sqrt(number);
+		
+		for (int f = 1; f <= sq; f++) {
+			//loop up to the square root
+			
+			if (number % f == 0) {
+				
+				divisors.add(f);
+				
+				if (f != sq) {
+					divisors.add(number/f);
+				}
+				
+			}
+		}
+		
+		Collections.sort(divisors);
+		
+		ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, divisors);
+		
+		ListView mListView = (ListView) findViewById(android.R.id.list);		
+		
+		mListView.setAdapter(adapter);
+		
+		mListView.setOnItemClickListener(this);
+	}
+	
+	@Override
+	public void onItemClick(AdapterView<?> list, View view, int position, long id) {
+		setResult((Integer) list.getAdapter().getItem(position));
+		this.finish();
 	}
 	
 }
--- src/com/makeramen/divisors/NumbersActivity.java
+++ src/com/makeramen/divisors/NumbersActivity.java
@@ -3,8 +3,9 @@ package com.makeramen.divisors;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -13,23 +14,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class NumbersActivity extends Activity implements ListView.OnItemClickListener {
+public class NumbersActivity extends ListActivity implements ListView.OnItemClickListener {
 	
 	public static final String LABEL_DIVISORS = " divisors";
 	public static final String LABEL_PRIME = "prime number";
 	public static final String LABEL_SPECIAL = "special case";
 	public static final String KEY_NUMBER = "number";
 	public static final String KEY_DESCRIPTION = "description";
-	public static final String KEY_PREFS = "preferences";
+	public static final String EXTRA_NUMBER = "number";
+	public static final String EXTRA_DIVISORS = "divisors";
+	public static final int INTENT_CODE = 0;
 	
 	ListView mListView;
 	SharedPreferences mPreferences;
 	SimpleAdapter mAdapter;
 	ListNumberBinder mBinder;
-	ArrayList<HashMap<String, String>> divisors = new ArrayList<HashMap<String, String>>();
+	ArrayList<HashMap<String, String>> mArrayList = new ArrayList<HashMap<String, String>>();
 	DivisorTask mThread;
 	ProgressDialog progressDialog;
+	Intent mIntent;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -46,7 +51,7 @@ public class NumbersActivity extends Activity implements ListView.OnItemClickLis
 		
 		mAdapter = new SimpleAdapter(
 				this,
-				divisors,
+				mArrayList,
 				R.layout.number_list_item,
 				new String[] {KEY_NUMBER, KEY_DESCRIPTION},
 				new int[] {android.R.id.text1, android.R.id.text2});
@@ -72,14 +77,34 @@ public class NumbersActivity extends Activity implements ListView.OnItemClickLis
 		super.onDestroy();
 	}
 
-
 	@Override
-	public void onItemClick(AdapterView<?> mList, View view, int position, long id) {
-		// TODO Auto-generated method stub
+	public void onItemClick(AdapterView<?> list, View view, int position, long id) {
 		
+		// get the label of clicked textview
+		TextView mTextView = (TextView)view.findViewById(android.R.id.text2);
+		String label = mTextView.getText().toString();
+		
+		if (label.equals(LABEL_PRIME) || label.equals(LABEL_SPECIAL)) {
+			// if prime or special, do nothing
+			return;
+		} else {
+			// if not prime or special, show divisors
+			mIntent = new Intent(this, DivisorsActivity.class);
+			
+			// pass the number that was clicked
+			mIntent.putExtra(EXTRA_NUMBER, position + 1);
+			// pass the number of factors it had
+//			mIntent.putExtra(EXTRA_DIVISORS, Integer.parseInt(label.substring(0, label.length() - LABEL_DIVISORS.length())));
+			
+			startActivityForResult(mIntent, INTENT_CODE);
+		}
 	}
-
-
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		mListView.setSelection(resultCode-1);
+	}
+	
 	/** Thread for creating list of numbers and number of divisors **/
 	private class DivisorTask extends AsyncTask<Void, Integer, Void> {
 
@@ -103,15 +128,14 @@ public class NumbersActivity extends Activity implements ListView.OnItemClickLis
 		protected Void doInBackground(Void... params) {
 			
 			int time = 0;	//used to track ui thread updates
-			
-			
+				
 			// BEGIN special case for 1
 			mMap = new HashMap<String , String>();
 			mMap.put(KEY_NUMBER, "1");
 			
 			mMap.put(KEY_DESCRIPTION, "special case");
 			
-			divisors.add(mMap);
+			mArrayList.add(mMap);
 			// END special case
 			
 			
@@ -137,7 +161,7 @@ public class NumbersActivity extends Activity implements ListView.OnItemClickLis
 				mMap.put(KEY_DESCRIPTION, (fcount == 2) ? LABEL_PRIME :(Integer.toString(fcount) + LABEL_DIVISORS));
 				
 				// add hashmap to arraylist
-				divisors.add(mMap);
+				mArrayList.add(mMap);
 				
 				
 				if (n % 379 == 0 && System.currentTimeMillis() - startTime > time){
